a7cfed3f49409d4ada9e6f491c663e512ac6acc3
Garrett Hufstedler <garretthufstedler@gmail.com>
2023-02-21 01:27:32 -0500

    Add header, navbar, and homepage image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('test');
+import './style.css'; 
+import './initialLoad.js';
+
+function divCreator() {
+    const element = document.createElement('div'); 
+    element.classList.add('hello');
+    return element
+}
+document.getElementById('content').appendChild(divCreator());
+// getElementById('content')
